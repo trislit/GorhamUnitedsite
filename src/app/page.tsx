0d924d07128cf88ff7c637ed25630666cebfa0df
Trislit <trislit@hotmail.com>
2026-03-12 16:38:54 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Navigation />
       <PetitionBanner />
-      <main>
+      <main className="pt-28 sm:pt-32">
         <Hero />
         <About />
         <TakeAction />
